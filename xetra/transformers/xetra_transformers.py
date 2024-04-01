7cfc6f8460039a8d6cf61e1e69e0df1410e7e451
Gaurav Gurjar <ggurjar333@gmail.com>
2024-04-01 18:29:21 +0530

    initial commit

---
+++ xetra/transformers/xetra_transformers.py
@@ -0,0 +1,68 @@
+"""Xetra ETL Component"""
+from typing import NamedTuple
+
+from xetra.common.s3 import S3BucketConnector
+
+
+class XetraSourceConfig(NamedTuple):
+    """
+    Class for source configuration data
+
+    src_first_extract_date: determines the date for extracting the source.
+    src_columns: source column names
+    src_col_date: column name for date in source
+    src_col_isin: column name for isin in source
+    src_col_time: column name for time in source
+    src_col_start_price: column name for starting price in source
+    src_col_min_price: column name for minimum price in source
+    src_col_max_price: column name for maximum price in source
+    src_col_traded_vol: column name for traded volume in source
+    """
+    src_first_extract_date: str
+    src_columns: list
+    src_col_date: str
+    src_col_isin: str
+    src_col_time: str
+    src_col_start_price: str
+    src_col_min_price: str
+    src_col_max_price: str
+    src_col_traded_vol: str
+
+
+class XetraTargetConfig(NamedTuple):
+    """
+    Class for target configuration data
+
+    trg_col_isin: column name for isin in target
+    trg_col_date: column name for date in target
+    trg_col_op_price: column name for opening price in target
+    trg_col_clos_price: column name for closing price in target
+    trg_col_min_price: column name for minimum price in target
+    trg_col_max_price: column name for maximum price in target
+    trg_col_dail_trad_vol: column name for daily traded volume in target
+    trg_col_ch_prev_clos: column name for change to previous day's closing price in target
+    trg_key: basic key of target file
+    trg_key_date_format: date format of target file key
+    trg_format: file format of the target file
+    """
+    trg_col_isin: str
+    trg_col_date: str
+    trg_col_op_price: str
+    trg_col_clos_price: str
+    trg_col_min_price: str
+    trg_col_max_price: str
+    trg_col_dail_trad_vol: str
+    trg_col_ch_prev_clos: str
+    trg_key: str
+    trg_key_date_format: str
+    trg_format: str
+
+
+class XetraETL():
+    """
+    Reads the Xetra data, transforms and writes the transformed to target
+    """
+
+    def __init__():
+
+
